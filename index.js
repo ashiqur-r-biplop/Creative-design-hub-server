6c27058f5390d293e2677c93a549dd2aa3cfd280
Ashiqur Rahman Biplop <biplops22@gmail.com>
2023-06-10 19:41:48 +0600

    update class

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 // middleware
 app.use(cors());
 app.use(express.json());
@@ -166,13 +166,44 @@ async function run() {
       const result = await classCollection.insertOne(body);
       res.send(result);
     });
-    app.get("/getClass/:email", verifyJWT, verifyInstructor, async (req, res) => {
-      const email = req.params.email;
-      const query = { instructorEmail: email };
-      const result = await classCollection.find(query).toArray();
+    app.get(
+      "/getClass/:email",
+      verifyJWT,
+      verifyInstructor,
+      async (req, res) => {
+        const email = req.params.email;
+        const query = { instructorEmail: email };
+        const result = await classCollection.find(query).toArray();
+        res.send(result);
+      }
+    );
+    app.put("/updateClass/:id", async (req, res) => {
+      const id = req.params.id;
+      const body = req.body;
+      console.log(body);
+      const query = { _id: new ObjectId(id) };
+      const options = { upsert: true };
+      const updateClass = {
+        $set: {
+          imgURL: body.imgURL,
+          className: body.className,
+          price: parseFloat(body.price),
+          availableSeats: parseFloat(body.availableSeats),
+        },
+      };
+      const result = await classCollection.updateOne(
+        query,
+        updateClass,
+        options
+      );
       res.send(result);
     });
-
+    app.delete("/deleteClass/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await classCollection.deleteOne(query);
+      res.send(result);
+    });
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
